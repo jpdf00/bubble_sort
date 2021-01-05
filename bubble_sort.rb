a43330dc6834adf12c122b787892ca7386b495cf
aliciapaz <aliciapazrojas@gmail.com>
2021-01-05 20:01:47 -0300

    add bubble_sort_yield method

--- bubble_sort.rb
+++ bubble_sort.rb
@@ -17,4 +17,34 @@ def bubble_sort(array)
   p array
 end
 
-bubble_sort([4,3,78,2,0,2])
+i = 0
+def bubble_sort_yield(array)
+  count = 1
+  until count == 0
+    count = 0
+    i = 0
+    until i == array.length - 1
+      temp = bubble_sort_by(array) do |fst, snd|
+        fst - snd
+      end
+      if temp > 1
+        a = array[i+1]
+        array[i+1] = array[i]
+        array[i] = a
+        count +=1
+      end
+    end
+      i += 1
+  end
+  p array
+end
+
+def bubble_sort_by(array)
+  yield array[i]
+  yield array[i+1]
+end
+
+
+
+# bubble_sort_by([4,3,78,2,0,2])
+bubble_sort_yield([4,3,78,2,0,2])

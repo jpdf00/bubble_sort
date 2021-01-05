3c91faa4f7b9d1436d1867b1b2a8084ddb53e3d3
jpdf00 <j.pdf00@gmail.com>
2021-01-05 19:35:19 -0300

    Update bubble_sort.rb
    Finished code

--- bubble_sort.rb
+++ bubble_sort.rb
@@ -1,3 +1,23 @@
-def bubble_sort(array)
+#require "pry"
 
+def bubble_sort(array)
+  i = 0
+  count = 1
+  until count == 0
+    #binding.pry
+    count = 0
+    i = 0
+    until i == array.length - 1
+      if array[i] > array[i+1]
+        a = array[i+1]
+        array[i+1] = array[i]
+        array[i] = a
+        count +=1
+      end
+      i += 1
+    end
+  end
+  p array
 end
+
+bubble_sort([4,3,78,2,0,2])
